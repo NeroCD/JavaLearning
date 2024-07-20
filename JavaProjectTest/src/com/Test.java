e325666583cb8fd92939e1da3b059197d28d65ea
NeroCD <lk2937@qq.com>
2024-07-20 22:15:29 +0800

    up

--- JavaProjectTest/src/com/Test.java
+++ JavaProjectTest/src/com/Test.java
@@ -1,21 +1,41 @@
 package com;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
-        Son s = new Son();
-        s.say(1);
+    public static void main(String[] args) {
+        //有父类引用指向子类对象
+        Animal a = new Cat();
+        a.eat();
+//      a.playGame();
+        System.out.println(a.age);
+
+
+        //向下转型
+        Cat c = (Cat)a;
+        c.eat();
+        c.playGame();
+        System.out.println(c.age);
+    }
+    
+}
+
+class Animal {
+    public int age = 40;
+
+    public void eat() {
+        System.out.println("动物吃东西");
     }
 }
 
-class Daddy {
-    public void say (int i) {
-        System.out.println("I am Daddy");
-    }
-}
+class Cat extends Animal {
+    public int age = 20;
+    public int weight = 10;
 
-class Son extends Daddy {
     @Override
-    public void say (int i) {
-        System.out.println("I am son");
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+    public void playGame() {
+        System.out.println("猫捉迷藏");
     }
 }
